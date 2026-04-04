c7e50ff71f4c5fdfc9c2557e7b9d82cf20f4006b
Jose <jcondotuanama@gmail.com>
2026-04-04 21:50:45 +0200

    Pure code upload - No data

---
+++ scripts/picos.py
@@ -0,0 +1,18 @@
+import wfdb as wf
+import numpy as np
+
+data_path = 'C:/Users/jm635/Documents/Proyecto_ECG/data/15814'
+
+record = wf.rdrecord(data_path)
+
+signals = record.p_signal
+fs = record.fs
+
+signal = signals[:fs*5, 0]
+
+threshold = np.mean(signal) + 1.5*np.std(signal)
+
+peaks = np.where(signal > threshold)[0]
+
+print("Primeros picos detectados:", peaks[:20])
+print("Número total:", len(peaks))
---
+++ scripts/plot_ecg.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import wfdb as wf
+import pandas as pd
+###Path to the ECG DATA. 
+Data_path = 'C:/Users/jm635/Documents/Proyecto_ECG/data/15814'
+#####Read the ECG data using wfdb
+record = wf.rdrecord(Data_path)
+
+###Extract the signal and the sampling frequency
+signals = record.p_signal
+sampling_frequency = record.fs
+
+####Grafic the first derivation of the ECG signal.
+
+plt.figure(figsize=(12, 8))                     # Crea una figura grande
+for i in range(signals.shape[1]):               # Itera por cada derivación
+    plt.subplot(signals.shape[1], 1, i+1)      # Divide la figura en subplots
+    plt.plot(signals[:, i])                    # Grafica la derivación i
+    plt.title(f'Derivación {i}')               # Pone título a cada subplot
+plt.tight_layout()                              # Ajusta los espacios entre gráficos                                      # Muestra la figura
+plt.xlabel("Time (samples)")
+plt.ylabel("Amplitude (mV)")
+print("Sampling frequency:", sampling_frequency)
+print("Signal shape:", signals.shape)
+print("Duration of ECG (seconds):", signals.shape[0] / sampling_frequency)
+plt.show()
+#Create a DataFrame with the ECG signals and save it as a CSV file.
+df = pd.DataFrame(signals, columns=[f'Derivación {i}' for i in range(signals.shape[1])])
+df.to_csv('C:/Users/jm635/Documents/Proyecto_ECG/data/ECG_signals.csv', index=False)
+
