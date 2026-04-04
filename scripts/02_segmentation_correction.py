f96fd8e4f42f2835c2d0a1f6b61072350f0c06f6
Jose <jcondotuanama@gmail.com>
2026-04-05 01:20:34 +0200

    Second correction of script 2

--- scripts/02_segmentation_correction.py
+++ scripts/02_segmentation_correction.py
@@ -8,11 +8,14 @@ record = wf.rdrecord(data_path)
 signals = record.p_signal
 fs = record.fs
 
-signal = -signals[:fs*5, 0]
+signal = -signals[:fs*5, 0] ##We invert the signal because in this particular record, the peaks are negative. This is just a matter of convention and it does not affect the analysis. We can adjust this if we want to work with the original signal.  
 
-threshold = np.mean(signal) + 2*np.std(signal)
 
-peaks = np.where(signal > threshold)[0]
+threshold = np.mean(signal) + 2*np.std(signal) #This is the umbral that we will use to detect the peaks. We can adjust it if we want to be more or less strict in our detection.    
+
+
+peaks = np.where(signal > threshold)[0] #Here we detect the peaks by finding the indices where the signal is greater than the threshold. This will give us a list of indices where the peaks are located. We can then use these indices to extract the corresponding values from the signal if we want to analyze them further. 
+
 
 # 4. FILTRO OF ENGIINERGIN: We want to keep only the peaks that are at least 0.5 seconds apart (to avoid detecting the same heartbeat multiple times)   
 real_peaks = []
